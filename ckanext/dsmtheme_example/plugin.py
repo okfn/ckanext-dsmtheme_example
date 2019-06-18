f31bf8d4c6921afd606b313b21baca0ecb291316
amercader <amercadero@gmail.com>
2019-06-18 15:53:14 +0200

    Register the extension folder needed
    This will make CKAN aware of them and will add them to the search path.
    
    There are three folders registered (paths are relative to the plugin.py
    file):
    
    * Templates (templates)
    * Static files (public)
    * Compiled assets (fanstatic)

--- ckanext/dsmtheme_example/plugin.py
+++ ckanext/dsmtheme_example/plugin.py
@@ -8,5 +8,6 @@ class Dsmtheme_ExamplePlugin(plugins.SingletonPlugin):
     # IConfigurer
 
     def update_config(self, config_):
-        pass
-
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'dsmtheme_example')
